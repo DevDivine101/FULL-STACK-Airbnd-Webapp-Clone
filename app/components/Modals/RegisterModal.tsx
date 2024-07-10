9284a27d97767c7a54928a15b2fe9515ae79b0c4
Oscdivin <divineuwandu84@Gmail.com>
2024-07-10 19:13:01 +0100

    update is on going now

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -3,7 +3,9 @@
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
-import { useCallback, useState } from "react";
+import {
+  //  useCallback,
+    useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import Modal from "./Modal";
